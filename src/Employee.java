2f681684794a2e7b56d845e405dc483d78b48267
Julio Simoes <jrsg.simoes@gmail.com>
2022-10-10 19:57:14 +0100

    generate class from class diagram

---
+++ src/Employee.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Employee extends UniversityMember {
+
+    /**
+     * Default constructor
+     */
+    public Employee() {
+    }
+
+    /**
+     * 
+     */
+    private int aactNo;
+
+    /**
+     * 
+     */
+    public int getAcctNo() {
+        // TODO implement here
+        return 0;
+    }
+
+}
